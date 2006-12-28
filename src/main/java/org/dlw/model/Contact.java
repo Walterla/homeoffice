3e0fe6fd0a098cb624e84607381ba53d8d1ab793
dlwhitehurst <dlwhitehurst@c661bcbd-d724-0410-982a-2b61a1c81e95>
2006-12-28 21:39:46 +0000

    added phone and email fields

--- src/main/java/org/dlw/model/Contact.java
+++ src/main/java/org/dlw/model/Contact.java
@@ -1,18 +1,30 @@
 package org.dlw.model;
 
+import org.appfuse.model.BaseObject;
+
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Entity;
+import java.io.Serializable;
 
 /**
  * @author dlwhitehurst 
  */
 @Entity
-public class Contact {
+public class Contact extends BaseObject implements Serializable {
+
     Long id;
-    String firstName;
-    String lastName;
+    protected String firstName;
+    protected String lastName;
+    protected String homePhone;
+    protected String workPhone;
+    protected String cellPhone;
+    protected String homeEmail;
+    protected String workEmail;
+
+
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -38,5 +50,56 @@ public class Contact {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public void setWorkPhone(String workPhone) {
+        this.workPhone = workPhone;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public String getHomeEmail() {
+        return homeEmail;
+    }
+
+    public void setHomeEmail(String homeEmail) {
+        this.homeEmail = homeEmail;
+    }
+
+    public String getWorkEmail() {
+        return workEmail;
+    }
+
+    public void setWorkEmail(String workEmail) {
+        this.workEmail = workEmail;
+    }
+
+    public String toString() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public boolean equals(Object object) {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public int hashCode() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
--- src/main/webapp/WEB-INF/pages/contact.jsp
+++ src/main/webapp/WEB-INF/pages/contact.jsp
@@ -13,6 +13,21 @@
     <s:textfield label="Last Name" name="contact.lastName"
                  value="%{contact.lastName}" required="true" cssClass="text medium"/>
 
+    <s:textfield label="Home Phone" name="contact.homePhone"
+                 value="%{contact.homePhone}" required="false" cssClass="text medium"/>
+
+    <s:textfield label="Work Phone" name="contact.workPhone"
+                 value="%{contact.workPhone}" required="false" cssClass="text medium"/>
+
+    <s:textfield label="Cell Phone" name="contact.cellPhone"
+                 value="%{contact.cellPhone}" required="false" cssClass="text medium"/>
+
+    <s:textfield label="Personal Email" name="contact.homeEmail"
+                 value="%{contact.homeEmail}" required="false" cssClass="text medium"/>
+
+    <s:textfield label="Work Email" name="contact.workEmail"
+                 value="%{contact.workEmail}" required="false" cssClass="text medium"/>
+
     <li class="buttonBar left">
         <s:submit method="save" value="Save" cssClass="button"/>
         <s:submit method="delete" value="Delete" cssClass="button"/>
