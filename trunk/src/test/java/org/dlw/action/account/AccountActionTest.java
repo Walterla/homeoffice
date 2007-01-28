15d283ad467697a483c25c36fe96051d588b4e3f
dlwhitehurst <dlwhitehurst@c661bcbd-d724-0410-982a-2b61a1c81e95>
2007-01-28 00:03:36 +0000

    added AccountActionTest and pom change for appfuse-maven-plugin

---
+++ trunk/src/test/java/org/dlw/action/account/AccountActionTest.java
@@ -0,0 +1,11 @@
+package org.dlw.action.account;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: piratepete
+ * Date: Jan 27, 2007
+ * Time: 10:13:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccountActionTest {
+}
